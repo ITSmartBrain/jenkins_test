1c15e8c404732aac8491cc5d42f528b3bd699713
al.tukanov@gmail.com <al.tukanov@gmail.com>
2022-10-19 15:18:18 +0400

    init3

--- src/main/java/dockertest/demo/DemoApplication.java
+++ src/main/java/dockertest/demo/DemoApplication.java
@@ -2,12 +2,16 @@ package dockertest.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.function.DoubleToIntFunction;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run2(DemoApplication.class, args);
+        System.out.println("test");
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
