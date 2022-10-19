70c752044c7010acfe973f245f86ead5f0c1257a
al.tukanov@gmail.com <al.tukanov@gmail.com>
2022-10-19 14:59:39 +0400

    init

--- src/main/java/dockertest/demo/FruitRepi.java
+++ src/main/java/dockertest/demo/FruitRepi.java
@@ -1,6 +1,6 @@
-package dockertest.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FruitRepi extends JpaRepository<Fruit, Integer> {
-}
+//package dockertest.demo;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface FruitRepi extends JpaRepository<Fruit, Integer> {
+//}
